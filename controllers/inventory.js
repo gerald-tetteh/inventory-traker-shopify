cb48c6f0225bac30e99f8ca86df92e5966bea075
Gerald Addo-Tetteh <geraldadt@outlook.com>
2022-01-08 11:12:52 +0000

    add view all page && add create and edit page

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -1,9 +1,59 @@
+const fetch = require("node-fetch");
+
 const constants = require("../utils/constants");
 
 exports.getAllInventory = (req, res, next) => {
   fetch(constants.dbUrl)
     .then((response) => response.json())
     .then((inventory) => {
-      res.render("view-inventory", { inventory });
-    });
+      res.render("index", { inventory });
+    })
+    .catch((err) => console.log(err));
 };
+exports.getCreateItem = (req, res, next) => {
+  res.render("create-edit-item", { edit: false, item: {}, messages: [] });
+};
+exports.postCreateItem = (req, res, next) => {
+  const item = {
+    name: req.body.name,
+    description: req.body.description,
+    unitPrice: req.body.unitPrice,
+    quantity: req.body.quantity,
+    serialNo: req.body.serialNo,
+  };
+  fetch(`${constants.dbUrl}?name=${item.name}`)
+    .then((response) => response.json())
+    .then((items) => {
+      if (items.length > 0) {
+        req.flash("Error", "Item with the same name already exits.");
+        res.status(422).render("create-edit-item", {
+          edit: false,
+          item,
+          messages: req.flash("Error"),
+        });
+      } else {
+        fetch(constants.dbUrl, {
+          method: "POST",
+          body: JSON.stringify(item),
+          headers: { "Content-Type": "application/json" },
+        })
+          .then((response) => {
+            if (response.ok) {
+              res.status(201).redirect("/");
+            }
+          })
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => console.log(err));
+};
+exports.getEditItem = (req, res, next) => {
+  const id = req.params.id;
+  fetch(`${constants.dbUrl}/${id}`)
+    .then((response) => response.json())
+    .then((item) => {
+      res.render("create-edit-item", { edit: true, item, messages: [] });
+    })
+    .catch((err) => console.log(err));
+};
+exports.postEditItem = (req, res, next) => {};
--- routes/inventory.js
+++ routes/inventory.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const { route } = require("express/lib/router");
 
 const inventoryController = require("../controllers/inventory");
 
 const router = express.Router();
 
 router.get("/", inventoryController.getAllInventory);
+router.get("/create", inventoryController.getCreateItem);
+router.post("/create", inventoryController.postCreateItem);
+router.get("/edit/:id", inventoryController.getEditItem);
+router.post("/edit", inventoryController.postEditItem);
 
 module.exports = router;
